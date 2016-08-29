6158933f719f69e83186938809669206ec4498fe
Vitor Paiva <vitorapaiva@gmail.com>
2016-08-29 09:47:14 -0300

    Bugfix on day04
    Bugfix on the exercise that was impending it from being accepted in hacker rank 100%. It was as break line that was different from the expected result

--- hackerrank/30daysofcode/day04.php
+++ hackerrank/30daysofcode/day04.php
@@ -8,30 +8,34 @@ class Person
         if($initialAge<0)
         {
             $initialAge=0;
-            echo 'Age is not valid, setting age to 0.';
+            echo "Age is not valid, setting age to 0.";
+            echo "\n";
         }
         $this->age=$initialAge;
     }
     public function amIOld()
-    {        
+    {
         if($this->age<13)
         {
-            echo "\nYou are young.";
+            echo "You are young.";
+            echo "\n";
         }
         else if($this->age>=13 && $this->age<18)
         {
-            echo "\nYou are a teenager.";
+            echo "You are a teenager.";
+            echo "\n";
         }
         else if($this->age>=18)
         {
-            echo "\nYou are old.";
+            echo "You are old.";
+            echo "\n";
         }
     }
     public  function yearPasses()
     {
         $this->age=$this->age+1;
-    }  
-      
+    }
+
 }
 $T = intval(fgets(STDIN));
  for($i=0;$i<$T;$i++){
