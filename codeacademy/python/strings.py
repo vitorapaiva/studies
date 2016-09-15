8e0b2918db6e76839e86f64a8993949b5b03156f
Vitor Paiva <vitorapaiva@gmail.com>
2016-09-15 10:58:39 -0300

    Create strings.py

---
+++ codeacademy/python/strings.py
@@ -0,0 +1,9 @@
+# Write your code below, starting on line 3!
+
+my_string = "Satans hatred knows no end"
+print len(my_string)
+print my_string.upper()
+
+g = "Golf"
+h = "Hotel"
+print "%s, %s" % (g, h)
