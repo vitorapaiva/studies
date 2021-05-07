d4d47d2e83323155e3a8ee00cac95f5248fa0054
Vitor Paiva <vitor.paiva@picpay.com>
2021-05-07 10:34:05 -0300

    Estudos

---
+++ udemy/python/Funcoes.py
@@ -0,0 +1,21 @@
+def mensagem():
+    print("Texto da funcao")
+
+
+def mensagemComTexto(texto):
+    print(texto)
+
+
+def soma(a, b):
+    print(a + b)
+
+
+def somaComRetorno(a, b):
+    return (a + b)
+
+
+mensagem()
+mensagemComTexto('Funcao com parametro')
+soma(10, 11)
+r = somaComRetorno(3, 2)
+print(r)
